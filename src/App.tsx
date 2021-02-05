e9cbde733a4b3f43b2c8594b6d5ae7ec304fa3dc
Christopher <onlygamernerd@gmail.com>
2021-02-05 21:03:59 +0100

    underlying fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './dashboard/components/Dashboard';
+import { useAuth0 } from '@auth0/auth0-react';
+import Home from './home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const { isAuthenticated } = useAuth0()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" exact component={Dashboard} />
+          </>
+        ) : (
+          <>
+            <Route path="/" exact component={Home} />
+          </>
+        )}
+        </Switch>
+      </Router>
     </div>
   );
 }
